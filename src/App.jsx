7edbfd58ef251627bfa6ed38157c9d4fd85d8082
Nevena809 <nevena.milojevic@dev.telnet.rs>
2024-10-04 12:37:25 +0200

    useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChallengde from "./components/TimerChallengde.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallengde title="Easy" targetTime={1}></TimerChallengde>
+        <TimerChallengde title="Not easy" targetTime={5}></TimerChallengde>
+        <TimerChallengde
+          title="Getting tough"
+          targetTime={10}
+        ></TimerChallengde>
+        <TimerChallengde title="Pros only" targetTime={15}></TimerChallengde>
+      </div>
     </>
   );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export default function Player() {
   const playerName = useRef();
-  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
@@ -12,8 +12,8 @@ export default function Player() {
   return (
     <section id="player">
       <h2>
-        Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}
-        {/* Welcome {enteredPlayerName ?? "unknown entity"} */}
+        {/* Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"} */}
+        Welcome {enteredPlayerName ?? "unknown entity"}
       </h2>
       <p>
         <input ref={playerName} type="text" />
